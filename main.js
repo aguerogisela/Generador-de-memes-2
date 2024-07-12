33770a91b5dac5354b20ea29b07b11ea959961d5
Belen Agüero <aguerogisela03@gmail.com>
2024-07-12 18:24:25 -0300

    Funcionalidaes insertar y descargar imagen

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-//Drawer, cerrar y abrir
+// Drawer, abrir y cerrar
 
-document.getElementById("textButton").addEventListener("click", function () {
+document.getElementById("textButton").addEventListener("click", () => {
 	toggleDrawer("textDrawer", "imageDrawer");
 });
 
-document.getElementById("imageButton").addEventListener("click", function () {
+document.getElementById("imageButton").addEventListener("click", () => {
 	toggleDrawer("imageDrawer", "textDrawer");
 });
 
-document
-	.getElementById("closeTextDrawer")
-	.addEventListener("click", function () {
-		closeDrawer("textDrawer");
-	});
+document.getElementById("closeTextDrawer").addEventListener("click", () => {
+	closeDrawer("textDrawer");
+});
 
-document
-	.getElementById("closeImageDrawer")
-	.addEventListener("click", function () {
-		closeDrawer("imageDrawer");
-	});
+document.getElementById("closeImageDrawer").addEventListener("click", () => {
+	closeDrawer("imageDrawer");
+});
 
-function toggleDrawer(openDrawerId, closeDrawerId) {
+const toggleDrawer = (openDrawerId, closeDrawerId) => {
 	let openDrawer = document.getElementById(openDrawerId);
 	let closeDrawer = document.getElementById(closeDrawerId);
 
@@ -30,19 +26,48 @@ function toggleDrawer(openDrawerId, closeDrawerId) {
 		closeDrawer.style.width = "0";
 		openDrawer.style.width = "250px";
 	}
-}
+};
 
-function closeDrawer(drawerId) {
+const closeDrawer = (drawerId) => {
 	document.getElementById(drawerId).style.width = "0";
-}
+};
 
-//Modo Oscuro
-
-document.addEventListener("DOMContentLoaded", function () {
+// Modo Oscuro
+document.addEventListener("DOMContentLoaded", () => {
 	const modoOscuroButton = document.getElementById("modoOscuro");
 	modoOscuroButton.addEventListener("click", toggleDarkMode);
 
-	function toggleDarkMode() {
+	const toggleDarkMode = () => {
 		document.body.classList.toggle("dark-mode");
+	};
+});
+
+//Insertar imagen
+const divImg = document.getElementById("image");
+const fileInput = document.getElementById("fileInput");
+const downloadBtn = document.getElementById("downloadBtn");
+
+fileInput.addEventListener("change", (e) => {
+	const file = e.target.files[0];
+	if (file) {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const imgElement = document.getElementById("imgTag");
+			imgElement.src = e.target.result;
+		};
+		reader.readAsDataURL(file);
+		console.log(file);
 	}
+});
+
+//Descargar imagen
+downloadBtn.addEventListener("click", () => {
+	domtoimage
+		.toBlob(divImg)
+		.then((blob) => {
+			window.saveAs(blob, "meme.png");
+		})
+		.catch((error) => {
+			console.error("Error al descargar la imagen:", error);
+		});
 });
