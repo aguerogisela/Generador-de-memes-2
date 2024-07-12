df381e3ac41dcea84b5dc85d7146680623b7cf07
Belen Agüero <aguerogisela03@gmail.com>
2024-07-12 19:07:42 -0300

    Comence con las funcionalidades de los filtros

--- main.js
+++ main.js
@@ -33,13 +33,13 @@ const closeDrawer = (drawerId) => {
 };
 
 // Modo Oscuro
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
 	const modoOscuroButton = document.getElementById("modoOscuro");
 	modoOscuroButton.addEventListener("click", toggleDarkMode);
 
-	const toggleDarkMode = () => {
+	function toggleDarkMode() {
 		document.body.classList.toggle("dark-mode");
-	};
+	}
 });
 
 //Insertar imagen
@@ -70,4 +70,61 @@ downloadBtn.addEventListener("click", () => {
 		.catch((error) => {
 			console.error("Error al descargar la imagen:", error);
 		});
+});
+// Obtener la imagen y los controles deslizantes de filtros
+const img = document.getElementById("imgTag");
+const brillo = document.getElementById("brightnessFiltro");
+const opacidad = document.getElementById("opacityFiltro");
+const contraste = document.getElementById("contrastFiltro");
+const desenfoque = document.getElementById("blurFiltro");
+const escalaDeGrises = document.getElementById("grayscaleFiltro");
+const sepia = document.getElementById("sepiaFiltro");
+const hue = document.getElementById("hueFiltro");
+const saturacion = document.getElementById("saturateFiltro");
+
+// Función para aplicar filtros a la imagen
+const aplicarFiltros = () => {
+	img.style.filter = `
+    brightness(${brillo.value})
+    opacity(${opacidad.value})
+    contrast(${contraste.value}%)
+    blur(${desenfoque.value}px)
+    grayscale(${escalaDeGrises.value}%)
+    sepia(${sepia.value}%)
+    hue-rotate(${hue.value}deg)
+    saturate(${saturacion.value}%)
+  `;
+};
+
+// Agregar event listeners para cada filtro
+const filtros = [
+	brillo,
+	opacidad,
+	contraste,
+	desenfoque,
+	escalaDeGrises,
+	sepia,
+	hue,
+	saturacion,
+];
+
+filtros.forEach((filtro) => filtro.addEventListener("input", aplicarFiltros));
+
+// Botón para restablecer filtros
+const restablecerFiltrosBtn = document.querySelector(".restablecer-filtros");
+
+restablecerFiltrosBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+
+	// Restablecer los valores de los controles deslizantes
+	brillo.value = 1;
+	opacidad.value = 1;
+	contraste.value = 100;
+	desenfoque.value = 0;
+	escalaDeGrises.value = 0;
+	sepia.value = 0;
+	hue.value = 0;
+	saturacion.value = 100;
+
+	aplicarFiltros();
 });
